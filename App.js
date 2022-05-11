3c4ec0918e8bc07e658ae1f6f94e0ec0089e52c2
andremcqu <mcquade.andre@gmail.com>
2022-05-11 12:07:51 +0200

    I added a border to the picture, and changed the color and size of the text

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.Pictruebox}>
+        <Image
+          style={styles.firstPictrue}
+          source={require('./assets/rover.jpg')} />
+      </View>
+      <View style={styles.wordbox}>
+        <Text style={styles.words}>Le rover Opportunity</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flexDirection: 'row',
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  Pictruebox:{
+    flex: 3,
+  },
+  firstPictrue: {
+    height: 600,
+    width: '100%',
+    borderColor: 'black',
+    borderRadius: 15,
+    borderWidth: 5,
+  },
+  smallcontainer: {
+    flex: 1,
+  },
+  wordbox: {
+    flex: 1,
+  },
+  words: {
+    color: 'red',
+    fontSize: 20,
   },
 });
